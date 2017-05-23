58038a13e4b880f4dc1fe5e904e57fc44b1a3b3a
仇志停 <291532933@qq.com>
2017-05-23 15:39:34 +0800

    second commit

---
+++ app/src/main/java/com/example/emmaqiu/myfirstapp/DisplayPasswardActivity.java
@@ -0,0 +1,42 @@
+package com.example.emmaqiu.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayPasswardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_passward);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_PASSWORD);
+        TextView textview = new TextView(this);
+        textview.setTextSize(40);
+        textview.setText(message);
+
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textview);
+    }
+
+}
---
+++ app/src/main/java/com/example/emmaqiu/myfirstapp/MyActivity.java
@@ -0,0 +1,77 @@
+package com.example.emmaqiu.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE ="com.example.emmaqiu.myfirstapp.MESSAGE";
+    public final static String EXTRA_PASSWORD ="com.example.emmaqiu.myfirstapp.PASSWORD";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this,DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);//将message使用函数putExtra()传给intent
+        //键值对，键是EXTRA_MESSAGE，对应的值是message，键是不变的
+
+        startActivity(intent);
+
+
+    }
+
+    public void sendPassword(View view){
+        Intent intent = new Intent(this,DisplayPasswardActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_password);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_PASSWORD,message);
+        startActivity(intent);
+    }
+
+}
